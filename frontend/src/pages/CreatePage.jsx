ff0e35b230574f693161d2ef6a8e1ccba8f69ff3
Narayan Khanal <narayankhanal435@gmail.com>
2025-04-03 13:42:18 -0400

    add sonner toast after adding a product

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
 import { useProductStore } from "../store/product.js";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const CreatePage = () => {
-  const [isToastOpen, setIsToastOpen] = useState(false);
-  const [messageInfo, setMessageInfo] = useState("");
-  const [successInfo, setSuccessInfo] = useState(false);
   const [newProduct, setNewProduct] = useState({
     name: "",
     price: "",
     productImage: "",
   });
   const { createProduct } = useProductStore();
+  const navigate = useNavigate();
   const handleAddProduct = async () => {
     // console.log(newProduct);
     const { success, message } = await createProduct(newProduct);
     console.log(success);
     console.log(message);
-    setIsToastOpen(true);
-    setSuccessInfo(success);
-    setMessageInfo(message);
 
     // clear all input values after adding the product
     if (success) {
@@ -27,12 +24,12 @@ const CreatePage = () => {
         price: "",
         productImage: "",
       });
+      toast.success("You've successfully added a product!", {
+        action: { label: "Go Home", onClick: () => navigate("/") },
+      });
+    } else {
+      toast.success("Error while adding a product!");
     }
-
-    //using setTimeout to display the toaster for only 5 seconds
-    setTimeout(() => {
-      setIsToastOpen(false);
-    }, 2500);
   };
   return (
     <main className="min-h-[90vh] max-w-[85%] mx-auto relative">
@@ -83,20 +80,6 @@ const CreatePage = () => {
           Add Product
         </button>
       </section>
-      {isToastOpen &&
-        (successInfo ? (
-          <section className="toaster-added sm:w-[30%] p-2 from-cyan-600 to-green-500 bg-gradient-to-r text-white absolute left-1/2 transform -translate-x-1/2 bottom-5 rounded-md flex flex-col gap-1">
-            {" "}
-            <p className="success text-lg font-bold sm:text-xl">Success</p>
-            <p className="message-info font-semibold">{messageInfo}</p>
-          </section>
-        ) : (
-          <section className="toaster-not-added sm:w-[30%] p-4 from-red-600 to-pink-500 bg-gradient-to-r text-white font-bold absolute left-1/2 transform -translate-x-1/2 bottom-5 rounded-md flex flex-col gap-1">
-            {" "}
-            <p className="success text-lg font-bold sm:text-xl">Error</p>
-            <p className="message-info font-semibold">{messageInfo}</p>
-          </section>
-        ))}
     </main>
   );
 };
